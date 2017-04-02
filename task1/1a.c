5616e6b539d77ea450d364dc55649a72e17fae92
abadied <abadi.eden@gmail.com>
2017-04-02 22:12:42 +0300

    fixed bugs

--- task1/1a.c
+++ task1/1a.c
@@ -8,31 +8,47 @@ struct virus {
     char name[16];
     char signature[];
 };
+struct virus *allocate_virus(unsigned short len){
+    virus *v = malloc(sizeof *v + len*sizeof (*v).signature[0]);
+    if(!v){
+        exit(EXIT_FAILURE);
+    }
+    (*v).length = len;
+    unsigned short ix;
+    for(ix = 0;ix < len; ix++)
+        (*v).signature[ix] = 0;
+
+    return v;
+}
 
 void PrintHex(char* buffer, int length){
     int i;
     for(i = 0;i < length; i++){ 
-        printf("%hhx ",buffer[i]);
+        printf("%hhX ",buffer[i]);
     }
 }
 
 
 int main(int argc, char** argv){
     void* endian = (char*)malloc(2*sizeof(char));
-    virus* v = (virus*)(malloc(sizeof(virus)));;
+    char length[2] = {'0','0'};
+    virus* v = allocate_virus(80);
     FILE* f = fopen(argv[1],"r");
     
     fread(endian,sizeof(char),2,f);
-    fread((void*)(&(*v).length),sizeof(char),2,f);
+    fread(length,sizeof(char),2,f);
+    
+    printf("%hu",(*v).length);
+    
+    printf("%hu",(*v).length);
     fread((void*)((*v).name),sizeof(char),16,f);
     
     fread((void*)((*v).signature),sizeof(char),80,f);
     PrintHex((char*)endian,2);
-    printf("%d \n",(*v).length);
+    
   
-    
     printf("%s \n",(*v).name);
-    
+    PrintHex((*v).signature,(int)(*v).length);
     free(endian);
     free(v);
     return 0;
